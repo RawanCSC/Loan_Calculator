c04f24145d51d8f1cd4d99e9a654d405f6b53afb
RawanCSC <relgohary@my.waketech.edu>
2026-04-21 15:34:28 -0400

    Create Interest.java
    todays interest

---
+++ Interest.java
@@ -0,0 +1,12 @@
+public class Interest {
+    
+    // Your program begins with a call to main()
+    public static void main(String[] args)
+    {
+
+        int interest =5 
+
+        System.out.println(interest);
+
+    }
+}
