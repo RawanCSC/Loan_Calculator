1eb07e32e4217a60cdccf923488d71aefb630fc8
RawanCSC <relgohary@my.waketech.edu>
2026-04-21 15:27:05 -0400

    Add files via upload
    This class will calculate the multiplication of two integers.

---
+++ Calculate.java
@@ -0,0 +1,14 @@
+public class Calculate {
+    
+    // Your program begins with a call to main()
+    public static void main(String[] args)
+    {
+
+        int x = 5 ;
+        int y = 10 ;
+
+        int c = x * y ; 
+
+        System.out.println(c);
+    }
+}
